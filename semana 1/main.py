5144d68649491be24766114cc86b88f065ba3ba7
MatteoOlarte <mateoc09+2@gmail.com>
2026-04-17 18:43:50 -0500

    chore(project): reorganize repo structure (move files to semana 1)
    - Move `main.py` -> `semana 1/main.py`
    - Move `data/Titanic - train.csv` -> `semana 1/data/Titanic - train.csv`
    - Add `semana 1/main.ipynb`
    - Update `pyproject.toml`, `.vscode/settings.json`, and `uv.lock`

--- semana 1/main.py
+++ semana 1/main.py
@@ -1,9 +1,9 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
 
 # Leer el dataset
-data = pd.read_csv('data/Titanic - train.csv')
+data = pd.read_csv('semana 1/data/Titanic - train.csv')
 
 # El atributo 'Age', Cabin y Embarked tienen valores faltantes que debemos procesar
 f, ax = plt.subplots(1, 2, figsize=(20, 10))
@@ -57,25 +57,20 @@ sns.violinplot(data=data, x='Sex', y='Age', hue='Survived', split=True, ax=ax[1]
 # Manejar los datos faltantes para 'Age'
 # --------------------------------------------------------------------------------------------------
 
-# Se va a extraer los prefijos de los nombre para despues agruparlos
+# Se va a extraer los prefijos de cada uno de los nombres y en base a estos se va a imputar un estimado de la
+# edad faltante
 prefixes = data['Name'].str.extract(r'([A-Za-z]+)\.')[0]
 data['Prefix'] = prefixes
 
 pd.crosstab(data['Prefix'], data['Sex'])
 
-# Simplificar los prefijos
-# prefix_replacements = {
-#     'Capt': 'Mr', 'Col': 'Mr', 'Countess': 'Mrs', 'Don': 'Mr', 'Jonkheer': 'Mr',
-#     'Lady': 'Mrs', 'Major': 'Mr', 'Mlle': 'Miss', 'Mme': 'Mrs', 'Ms': 'Mrs',
-#     'Rev': 'Mr', 'Sir': 'Mr', 'Dr': 'Mr'
-# }
-# data['Prefix'] = data['Prefix'].replace(prefix_replacements)
-
-data['Prefix'].replace(
-    ['Capt','Col','Countess','Don','Jonkheer','Lady','Major','Mlle','Mme','Ms','Rev','Sir','Dr'],
-    ['Mr','Mr','Other','Mr','Other','Other','Mr','Miss','Mrs','Mrs','Mr','Mr','Mr'], 
-    inplace=True
+# Se va a simplificar el número de prefijos, renombrando aquellos que no son muy comunes o tienen una frecuencia baja
+# en base al contexto de su prefijo se le asignará uno nuevo dentro de las categorías 'Mr', 'Miss', 'Mrs' u 'Other'
+data['Prefix'] = data['Prefix'].replace(
+    ['Capt', 'Col', 'Countess', 'Don', 'Jonkheer', 'Lady', 'Major', 'Mlle', 'Mme', 'Ms', 'Rev', 'Sir', 'Dr'],
+    ['Mr', 'Mr', 'Other', 'Mr', 'Other', 'Other', 'Mr', 'Miss', 'Mrs', 'Mrs', 'Mr', 'Mr', 'Mr'],
 )
+
 
 data.groupby(['Prefix'])['Age'].mean()
 
@@ -87,7 +82,7 @@ data.loc[(data['Age'].isnull()) & (data['Prefix'] == 'Mrs'), 'Age'] = 35.80
 data.isnull().any()
 
 f, ax = plt.subplots(1, 2, figsize=(20, 10))
-data[data['Survived']==0].Age.plot.hist(ax=ax[0], bins=100)
+data[data['Survived'] == 0].Age.plot.hist(ax=ax[0], bins=100)
 ax[0].set_xticks(range(0, 80, 5))
-data[data['Survived']==1].Age.plot.hist(ax=ax[1], bins=100)
+data[data['Survived'] == 1].Age.plot.hist(ax=ax[1], bins=100)
 ax[1].set_xticks(range(0, 80, 5))
